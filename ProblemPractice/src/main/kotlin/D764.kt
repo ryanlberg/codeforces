61e16099ce80d5e7e21377add85764030b39a0cb
Ryan Berg <ryanmcberg@gmail.com>
2022-01-27 17:10:48 -0600

    added d764

---
+++ ProblemPractice/src/main/kotlin/D764.kt
@@ -0,0 +1,71 @@
+import java.io.PrintWriter
+import java.util.StringTokenizer
+import kotlin.math.*
+import kotlin.random.*
+import kotlin.collections.*
+
+private val INPUT = System.`in`
+private val OUTPUT = System.out
+
+private val bufferedReader = INPUT.bufferedReader()
+private val outputWriter = PrintWriter(OUTPUT, false)
+private  fun readLn() = bufferedReader.readLine()!!
+
+private fun readList() = readLn().split(' ')
+private var tokenizer = StringTokenizer("")
+private fun read(): String {
+    while(tokenizer.hasMoreTokens().not()) tokenizer = StringTokenizer(readLn(), " ")
+    return tokenizer.nextToken()
+}
+
+private fun readInt() = read().toInt()
+private fun readLong() = read().toLong()
+private fun readDouble() = read().toDouble()
+
+private fun readIntList() = readList().map { it.toInt() }
+private fun readLongList() = readList().map { it.toLong() }
+private fun readDoubleList() = readList().map { it.toDouble() }
+
+private fun readIntArray(n: Int = 0) =
+    if (n == 0) readList().run { IntArray(size) { get(it).toInt() } } else IntArray(n) { readInt() }
+
+private fun readLongArray(n: Int = 0) =
+    if (n == 0) readList().run { LongArray(size) { get(it).toLong() } } else LongArray(n) { readLong() }
+
+private fun readDoubleArray(n: Int = 0) =
+    if (n == 0) readList().run { DoubleArray(size) { get(it).toDouble() } } else DoubleArray(n) { readDouble() }
+
+
+private fun Int.modPositive(other: Int): Int = if (this % other < 0) ((this % other) + other) else (this % other)
+
+
+
+fun main(args: Array<String>) {
+    var cases : Int = readInt()
+    repeat(cases) {
+        var letterCount : Int = readInt()
+        var k : Int = readInt()
+        var letters = Array(26) {0}
+        var lets : String = readLn()
+        for(ch in lets) {
+            letters[ch-'a']++
+        }
+
+        var pairs = 0
+        var singles = 0
+
+        for(i in 0 until letters.size) {
+            pairs += letters[i]/2
+            singles += letters[i] % 2
+        }
+
+
+        var cando = 2 * (pairs / k);
+        singles += 2 * (pairs % k);
+
+        if(singles >= k) {
+            cando++
+        }
+        println(cando)
+    }
+}

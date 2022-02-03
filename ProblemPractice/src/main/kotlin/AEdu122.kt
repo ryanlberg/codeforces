64fc99c9b6ceaedc75ecc525fd9fad6f02172d92
Ryan Berg <ryanmcberg@gmail.com>
2022-02-02 19:29:37 -0600

    Added Aedu122

---
+++ ProblemPractice/src/main/kotlin/AEdu122.kt
@@ -0,0 +1,76 @@
+import java.io.PrintWriter
+import java.util.StringTokenizer
+import kotlin.math.*
+import kotlin.random.*
+import kotlin.collections.*
+
+private val INPUT = System.`in`
+private val OUTPUT = System.out
+
+private val bufferedReader = INPUT.bufferedReader()
+private val outputWriter = PrintWriter(OUTPUT, false)
+private  fun readLn() = bufferedReader.readLine()!!
+
+private fun readList() = readLn().split(' ')
+private var tokenizer = StringTokenizer("")
+private fun read(): String {
+    while(tokenizer.hasMoreTokens().not()) tokenizer = StringTokenizer(readLn(), " ")
+    return tokenizer.nextToken()
+}
+
+private fun readInt() = read().toInt()
+private fun readLong() = read().toLong()
+private fun readDouble() = read().toDouble()
+
+private fun readIntList() = readList().map { it.toInt() }
+private fun readLongList() = readList().map { it.toLong() }
+private fun readDoubleList() = readList().map { it.toDouble() }
+
+private fun readIntArray(n: Int = 0) =
+    if (n == 0) readList().run { IntArray(size) { get(it).toInt() } } else IntArray(n) { readInt() }
+
+private fun readLongArray(n: Int = 0) =
+    if (n == 0) readList().run { LongArray(size) { get(it).toLong() } } else LongArray(n) { readLong() }
+
+private fun readDoubleArray(n: Int = 0) =
+    if (n == 0) readList().run { DoubleArray(size) { get(it).toDouble() } } else DoubleArray(n) { readDouble() }
+
+
+private fun Int.modPositive(other: Int): Int = if (this % other < 0) ((this % other) + other) else (this % other)
+
+
+
+fun main(args: Array<String>) {
+    var cases : Int = readInt()
+    var sevens = mutableSetOf<String>()
+    for(i in 1 until 1000) {
+        if(i % 7 == 0){
+
+            sevens.add(Integer.toString(i))
+        }
+    }
+
+    repeat(cases) {
+        var cur = readInt().toString()
+        var curdif = ""
+        var curamount = Integer.MAX_VALUE
+        for(x in sevens) {
+            if(x.length != cur!!.length) {
+                continue
+            } else {
+                var count = 0
+                for(y in 0 until cur.length) {
+                    if(x[y] != cur[y]) {
+                        count++
+                    }
+                }
+                if(count < curamount) {
+                    curamount = count
+                    curdif = x
+                }
+            }
+        }
+
+        println(curdif)
+    }
+}
